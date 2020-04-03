2922e95af484e8ebc0fbd8d6ad91541c3e78f308
Qi Wenmin <qiwenmin@gmail.com>
2020-04-03 09:40:10 +0800

    Add tqdm.

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
 import argh
+from tqdm import tqdm
+
+@argh.named('tqdm')
+def tqdm_demo():
+    """tqdm - means "progress" in Arabic and is an abbreviation for "I love you so much" in Spanish."""
+
+    for _ in tqdm(range(int(1e7)), desc='Progress#1'):
+        pass
+
 
 def greet(name = 'world'):
     """Greeting to the world."""
@@ -15,6 +24,7 @@ def reply(name):
 
 if __name__ == '__main__':
     argh.dispatch_commands([
+        tqdm_demo,
         greet,
         reply,
         ])
